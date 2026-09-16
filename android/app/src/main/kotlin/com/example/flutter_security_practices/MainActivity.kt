ea6ae06567527691ae7b35c077cdd819a11dba61
ANGGORO RAVI <whyuravi.2008@gmail.com>
2026-09-16 10:24:37 +0700

    feat: setup project scaffolding for Flutter Security Practices

---
+++ android/app/src/main/kotlin/com/example/flutter_security_practices/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_security_practices
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,13 @@
+﻿import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_security_practices/main.dart';
+
+void main() {
+  testWidgets('Menu smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const SecurityPracticesApp());
+
+    // Verify that our menu items are found.
+    expect(find.text('Security Practices Demo'), findsOneWidget);
+    expect(find.text('1. Secure Storage'), findsOneWidget);
+  });
+}
